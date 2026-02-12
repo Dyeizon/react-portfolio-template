a61ebef0cae48b8c4652c7fe78fa939747d9391b
dyeizon <dyeizonque@gmail.com>
2026-02-12 18:36:01 -0300

    projs 4 and 5

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -2,6 +2,8 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import project01 from '../assets/images/project01.png';
 import project02 from '../assets/images/project02.png';
 import project03 from '../assets/images/project03.png';
+import project04 from '../assets/images/project04.png';
+import project05 from '../assets/images/project05.png';
 
 import '../assets/styles/Project.scss';
 import { faLock } from '@fortawesome/free-solid-svg-icons';
@@ -42,6 +44,22 @@ const proj3Stack = [
     "JWT",
 ];
 
+const proj4Stack = [
+    "React",
+    "TailwindCSS",
+    "Supabase",
+    "Weather API",
+    "Data Analysis",
+];
+
+const proj5Stack = [
+    "Node.js",
+    "Express.js",
+    "PostgreSQL",
+    "bcryptjs",
+    "MVC",
+];
+
 function Project() {
     return(
     <div className="projects-container" id="projects">
@@ -60,7 +78,7 @@ function Project() {
             <div className="project">
                 <a href={project02} target="_blank" rel="noreferrer"><img src={project02} className="zoom" alt="thumbnail" width="100%"/></a>
                 <h2><a href="https://github.com/Dyeizon/OrderManager" target="_blank" rel="noreferrer"><GitHubIcon/> Agiliza Aí</a></h2>
-                <p>Designed and developed a web-based order management system for restaurants and food service businesses. The application allows staff to register and organize customer orders, manage kitchen workflows using a drag-and-drop interface across order stages such as queue, in production, and ready, and handle payments via Pix. The system also displays ready orders on a public screen, improving order tracking, kitchen efficiency, and customer experience.<div className="flex-chips">
+                <p><b>Agiliza Aí</b> is a web-based order management system for restaurants and food service businesses. The application allows staff to register and organize customer orders, manage kitchen workflows using a drag-and-drop interface across order stages such as queue, in production, and ready, and handle payments via Pix. The system also displays ready orders on a public screen, improving order tracking, kitchen efficiency, and customer experience.<div className="flex-chips">
                     {proj2Stack.map((label, index) => (
                         <Chip key={index} className='chip' label={label} />
                     ))}
@@ -70,12 +88,33 @@ function Project() {
             <div className="project">
                 <a href={project03} target="_blank" rel="noreferrer"><img src={project03} className="zoom" alt="thumbnail" width="100%"/></a>
                 <h2><a href="https://github.com/Dyeizon/bau-da-saude" target="_blank" rel="noreferrer"><GitHubIcon/> Baú da Saúde</a></h2>
-                <p>Designed and developed a web application that allows patients to organize medical exams, register health metrics, visualize historical data through interactive time-based charts, and receive periodic email reminders for recurring medical exams.<div className="flex-chips">
+                <p>The web application <b>Baú da Saúde</b> allows patients to organize medical exams, register health metrics, visualize historical data through interactive time-based charts, and receive periodic email reminders for recurring medical exams.<div className="flex-chips">
                     {proj3Stack.map((label, index) => (
                         <Chip key={index} className='chip' label={label} />
                     ))}
                 </div></p>
             </div>
+
+            <div className="project">
+                <a href={project04} target="_blank" rel="noreferrer"><img src={project04} className="zoom" alt="thumbnail" width="100%"/></a>
+                <h2><a href="https://github.com/Dyeizon/bau-da-saude" target="_blank" rel="noreferrer"><GitHubIcon/> Vitis</a></h2>
+                <p>The <b>Vitis</b> web app supports viticulture analysis by correlating grape growth stages with environmental data such as temperature and humidity, retrieved automatically via APIs, to indicate potential fungal risks based on location and climatic conditions.<div className="flex-chips">
+                    {proj4Stack.map((label, index) => (
+                        <Chip key={index} className='chip' label={label} />
+                    ))}
+                </div></p>
+            </div>
+
+             <div className="project">
+                <a href={project05} target="_blank" rel="noreferrer"><img src={project05} className="zoom" alt="thumbnail" width="100%"/></a>
+                <h2><a href="https://github.com/Dyeizon/bau-da-saude" target="_blank" rel="noreferrer"><GitHubIcon/> BancaDeDados</a></h2>
+                <p><b>BancaDeDados</b> is a CRUD web application for organizing and managing clients, managers, products and purchases.<div className="flex-chips">
+                    {proj5Stack.map((label, index) => (
+                        <Chip key={index} className='chip' label={label} />
+                    ))}
+                </div></p>
+            </div>
+
 
             {/* <div className="project">
                 <a href="https://yujisatojr.itch.io/highspeedchase" target="_blank" rel="noreferrer"><img src={mock09} className="zoom" alt="thumbnail" width="100%"/></a>
